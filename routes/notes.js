29039de32c5ed967b202922bd429e30f09f48137
whitbreezy <whitneybrady20@gmail.com>
2023-11-05 14:24:23 -0700

    routes mostly working

---
+++ routes/notes.js
@@ -0,0 +1,51 @@
+//bring in router for notes
+const notes = require('express').Router();
+
+//bring in fs helper functions
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+} = require('../helpers/fsUtils.js');
+
+//retrieve all notes 
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// delete specific note by its title
+notes.delete('/:title', (req, res) => {
+    const title = req.params.title;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        
+        const result = json.filter((note) => note.title !== title);
+
+     
+        writeToFile('./db/db.json', result);
+
+
+        res.json(`Item ${title} has been deleted 🗑️`);
+    });
+});
+
+//post route for new note
+notes.post('/', (req, res) => {
+    console.log(req.body);
+    const { title, text } = req.body;
+    if (req.body) {
+      const newNote = {
+        title,
+        text
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+    } else {
+    res.error('Error in adding note');
+    }
+});
+
+module.exports = notes;
+
---
+++ server.js
@@ -0,0 +1,31 @@
+//bring in dependencies 
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
+//set up port for heroku
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+//get route for home page
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//get route for notes page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+//listen for server 
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
